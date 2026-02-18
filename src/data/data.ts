1ab896a10235c6d7f87670dbd981da286f8181a3
wrathyz <satayu132@gmail.com>
2026-02-18 21:39:19 +0700

    feat: refactor movie data structure

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,9 +1,16 @@
-export const movies: {
+type TSource = {
   title: string
   slug: string
   thumb: string
+  cover: string
   release_date: string
   runtime: string
-  type: 'show' | 'movie'
+  type: (string & {}) | 'show' | 'movie'
   files: string[]
-}[] = []
+}
+
+export const movies = [].map((v: TSource) => ({
+  ...v,
+  thumb: '/api/images/' + v.slug + v.thumb,
+  cover: '/api/images/' + v.slug + v.cover,
+}))
